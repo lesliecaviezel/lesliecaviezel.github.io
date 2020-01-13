170294b2305d1a38eab4928c752630bc90f154b2
Yining Xu <yining.xu@linkplay.com>
2020-01-13 19:15:19 +0800

    增加页面本地化储存，防止页面刷新，首页宽度还原

--- js/nav.js
+++ js/nav.js
@@ -1,26 +1,32 @@
 window.onload = function () {
-    // 1 获取需要的标签
+    let webOpenContent = sessionStorage.getItem("webState");
+    // 获取需要的标签
     let list = $('navlist');
     let nav = list.children[0];
     let allItemA = nav.children;
     let timer = null;
     let sliderBar = this.document.getElementsByClassName("wrapper-sidebar")[0];
-    console.log(sliderBar);
-    console.log(list);
-    console.log(nav);
-    // 2 遍历
+
+    // 在当前页面刷新，如果已经是open的，直接设置为30%宽度
+    if (webOpenContent === "open"){
+        list.style.width = fixWidth(30) + "px"; 
+        console.log(fixWidth(30))
+        return;
+    }
+
+    // 遍历
     for (let i = 0; i < allItemA.length; i++) {
         let itemA = allItemA[i];
         console.log(itemA);
         itemA.onmousedown=function(){
             let barWidth = sliderBar.style.width;
-            console.log(barWidth)
             if(barWidth > fixWidth(30) || isNaN(parseInt(barWidth))){
+                sessionStorage.setItem("webState", "open"); 
                 let begin = 100, end = 30;
                 // 缓动动画
                 timer = setInterval(function () {
                     begin = begin + (end - begin) * 0.1;
-                    sliderBar.style.width = fixWidth(begin) + "px";
+                    sliderBar.style.width = fixWidth(parseInt(begin)) + "px";
                     if(parseInt(begin) === 30){
                         clearInterval(timer); 
                     }
